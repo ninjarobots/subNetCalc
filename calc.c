a28bf5ac280209936a7a18e717684d65a4810986
ninjarobots <47232950+ninjarobots@users.noreply.github.com>
2019-02-06 12:30:33 -0600

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,138 +1,111 @@
-
 /*
-    
-    I want a function that accepts two char * parameters. The first is an IP address, the second is a subnet.
+	
+	I want a function that accepts two char * parameters. The first is an IP address, the second is a subnet.
 
-    Without using the string library, determine the IP range that the IP address identified can communicate without
-        the use of a gateway.
+	Without using the string library, determine the IP range that the IP address identified can communicate without
+		the use of a gateway.
 
-    use BitWise operands, pointer arithmetic, and return a struct containing two in_addr_t variables by utilizing 
-        the netinet/in.h library. 
+	use BitWise operands, pointer arithmetic, and return a struct containing two in_addr_t variables by utilizing 
+		the netinet/in.h library. 
 
-    Ensure to do proper input validation and return -1 on invalid inputs.
+	Ensure to do proper input validation and return -1 on invalid inputs.
 
 */
 
 #include<stdlib.h>
-#include<string.h>
+#include<stdint.h>
 #include<stdio.h>
 #include<math.h>
 
 struct Network {
-    char* first;
-    char* last;
+	char* first;
+	char* last;
 };
 
 struct Network *netAddr(char* addr, char* subNet);
-int intAddr(char* string, int* first, int* second, int* third, int* fourth);
+int binMaker(char* string);
+char* makeString(uint32_t binValue);
 
 int main() {
-    char* addr = "122.13.14.1";
-    char* subNet = "255.128.0.0";
-    struct Network *networks = netAddr(addr, subNet);
+	char* addr = "122.13.15.18";
+	char* subNet = "255.255.128.0";
+	struct Network *networks = netAddr(addr, subNet);
+	getchar();
 }
 
 struct Network *netAddr(char* addr, char* subNet){
-    struct Network *returnStruct = malloc(sizeof(struct Network));
-    int* oct0 = (int)malloc(sizeof(int));
-    int* oct1 = (int)malloc(sizeof(int));
-    int* oct2 = (int)malloc(sizeof(int));
-    int* oct3 = (int)malloc(sizeof(int));
-    int* sub0 = (int)malloc(sizeof(int));
-    int* sub1 = (int)malloc(sizeof(int));
-    int* sub2 = (int)malloc(sizeof(int));
-    int* sub3 = (int)malloc(sizeof(int));
-    char firstAddr[51] = { 0 };
-    char lastAddr[51] = { 0 };
-    unsigned int bitShifter = 0xFF;
-    if (intAddr(addr, oct0, oct1, oct2, oct3) == -1) {
-        printf("INVALID INPUT");
-        return -1;
-    }
-    if (intAddr(subNet, sub0, sub1, sub2, sub3) == -1) {
-        printf("INVALID INPUT");
-        return -1;
-    }
-    if (*sub1 < 255) {
-        if (*sub3 != 0 || *sub2 !=0) {
-            printf("INVALID SUBNET MASK");
-            return -1;
-        }
-        snprintf(firstAddr, 50, "(%d.%d.%d.%d)", *oct0, (*oct1 & *sub1), 0, 1);
-        returnStruct->first = &firstAddr;
-        unsigned int wildcard = (bitShifter ^ *sub1);
-        snprintf(lastAddr, 50, "(%d.%d.%d.%d)", *oct0, (*oct1 | wildcard),255 , 254);
-        returnStruct->last = lastAddr;
-    }else if (*sub2 < 255) {
-        if (*sub3 != 0) {
-            printf("INVALID SUBNET MASK");
-            return -1;
-        }
-        snprintf(firstAddr, 50, "(%d.%d.%d.%d)", *oct0, *oct1, (*oct2 & *sub2), 1);
-        returnStruct->first = &firstAddr;
-        unsigned int wildcard = (bitShifter ^ *sub2);
-        snprintf(lastAddr, 50, "(%d.%d.%d.%d)", *oct0, *oct1, (*oct2 | wildcard), 254);
-        returnStruct->last = lastAddr;    
-    }
-    else if (*sub3 <= 255) {
-        snprintf(firstAddr, 50, "(%d.%d.%d.%d)", *oct0, *oct1, *oct2, ((*oct3 & *sub3) + 1));
-        returnStruct->first = &firstAddr;
-        unsigned int wildcard = (bitShifter ^ *sub3);
-        snprintf(lastAddr, 50, "(%d.%d.%d.%d)", *oct0, *oct1, *oct2, ((*oct3 | wildcard) - 1));
-        returnStruct->last = lastAddr;
-    }
-    return returnStruct;
+	struct Network *returnStruct = malloc(sizeof(struct Network));
+	uint32_t binAddr = binMaker(addr);
+	int subValue = 1;
+	if (binAddr == 0) {
+		printf("INVALID NETWORK ADDRESS");
+		return -1;
+	}
+	uint32_t binSubNet = binMaker(subNet);
+	if (binSubNet == 0) {
+		printf("INVALID SUBNET MASK");
+		return -1;
+	}
+	if (binSubNet == 0xFFFFFFFF) {
+		subValue = 0;
+	}
+	uint32_t wildCardAddr = (binSubNet ^ 0xFFFFFFFF);
+	uint32_t binFirst = ((binSubNet & binAddr) + subValue);
+	uint32_t binLast = (binFirst + (wildCardAddr)-(subValue + subValue));
+	returnStruct->first = makeString(binFirst);
+	returnStruct->last = makeString(binLast);
+	return returnStruct;
 }
 
-int intAddr(char* string, int* first, int* second, int* third, int* fourth) {
-    int tmp = 0;
-    double tensCount = 0;
-    int dotPosition = 0;
-    int lastDotPosition = 0;
-    int placeCounter = 0;
-    int count = 0;
-    for (int i = 0; i <= strlen(string); i++) {
-        if (string[i] != '.' && i != strlen(string)) {
-            count++;
-        }
-        else {
-            tensCount = pow(10, (count - 1));
-            for (int o = (i - count); o < i; o++) {
-                tmp += ((string[o] - 48) * tensCount);
-                tensCount /= 10;
-            }
-            if (tmp < 0 | tmp > 255) {
-                printf("INVALID ADDRESS");
-                return -1;
-            }
-            switch (dotPosition) {
-            case 0:
-                *first = tmp;
-                dotPosition++;
-                tmp = 0;
-                break;
-            case 1:
-                *second = tmp;
-                dotPosition++;
-                tmp = 0;
-                break;
-            case 2:
-                *third = tmp;
-                dotPosition++;
-                tmp = 0;
-                break;
-            case 3:
-                *fourth = tmp;
-                dotPosition++;
-                tmp = 0;
-                break;
-            }
-            tensCount = 0;
-            lastDotPosition = count + 2;
-            count = 0;
-        }
+int binMaker(char* string) {
+	uint32_t buffer = { 0 };
+	int tmp = 0;
+	double tensCount = 0;
+	int dotPosition = 0;
+	int lastDotPosition = 0;
+	int count = 0;
+	for (int i = 0; dotPosition < 4; i++) {
+		if (*(string + i) != '.' && *(string + i) != NULL) {
+			count++;
+		}
+		else {
+			tensCount = pow(10, (count - 1));
+			for (int o = (i - count); o < i; o++) {
+				tmp += ((*(string + o) - 48) * tensCount);
+				tensCount /= 10;
+			}
+			if (tmp < 0 | tmp > 255) {
+				return 0;
+			}
+			if (dotPosition < 3) {
+				buffer += tmp;
+				buffer = buffer << 8;
+				dotPosition++;
+				tmp = 0;
+			}
+			else if (dotPosition == 3) {
+				buffer += tmp;
+				dotPosition++;
+			}
+			tensCount = 0;
+			lastDotPosition = count + 2;
+			count = 0;
+		}
 
-    }
-    return 0; 
+	}
+	return buffer; 
 }
-
+char* makeString(uint32_t binValue) {
+	char* returnString = malloc(sizeof(char) * 21);
+	uint32_t checkString = binValue;
+	uint32_t andBit = 255;
+	int first = (checkString & andBit);
+	checkString = checkString >> 8;
+	int second = (checkString & andBit);
+	checkString = checkString >> 8;
+	int third = (checkString & andBit);
+	checkString = checkString >> 8;
+	int fourth = (checkString & andBit);
+	snprintf(returnString, 20, "(%d.%d.%d.%d)", fourth, third, second, first);
+	return returnString;
+}
